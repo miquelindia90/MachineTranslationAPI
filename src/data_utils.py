8386c2fdba6a845d5e7451c67544fdb0cb2f42f7
Miquel India <mindia@verbio.com>
2024-05-10 00:33:25 +0200

    Splitted MTTokenizer as a two SingleTokenizers

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,26 +1,49 @@
-
-
-def _read_text_sentences(file_path: str)-> list:
+def _read_text_sentences(file_path: str) -> list:
     sentences = list()
-    with open(file_path, 'r') as input_file:
+    with open(file_path, "r") as input_file:
         for line in input_file:
             sentences.append(line.strip())
     return sentences
 
 
-class DataIterator():
-    def __init__(self, source_path: str, target_path: str, metadata_path: str, language_pair: str = "None"):
+class SingleTokenizer:
+    def __init__(self):
+        self._tokens_dictionary = dict()
+
+    def get_tokens_dictionary(self) -> dict:
+        return self._tokens_dictionary
+
+
+class MTTokenizer:
+    def __init__(self):
+        self._source_tokenizer = SingleTokenizer()
+        self._target_dicitonary = SingleTokenizer()
+
+    def get_source_tokens_dictionary(self) -> dict:
+        return self._source_tokenizer.get_tokens_dictionary()
+
+    def get_target_tokens_dictionary(self) -> dict:
+        return self._target_dicitonary.get_tokens_dictionary()
+
+
+class DataIterator:
+    def __init__(
+        self,
+        source_path: str,
+        target_path: str,
+        metadata_path: str,
+        language_pair: str = "None",
+    ):
         self._source_path = source_path
         self._target_path = target_path
         self._metadata_path = metadata_path
         self._language_pair = language_pair
         self.source_list, self.target_list = self.__prepare_iterator_data()
-        
 
     def __extract_language_indexes(self) -> list:
         language_indexes = list()
-        languages = self._language_pair.split("-")        
-        with open(self._metadata_path, 'r') as metadata_file:
+        languages = self._language_pair.split("-")
+        with open(self._metadata_path, "r") as metadata_file:
             for index, line in enumerate(metadata_file.readlines()):
                 sline = line.strip().split('"')
                 if sline[3] == languages[0] and sline[7] == languages[1]:
@@ -37,12 +60,14 @@ class DataIterator():
         source_list = _read_text_sentences(self._source_path)
         target_list = _read_text_sentences(self._target_path)
         if self._language_pair != "None":
-            source_list, target_list = self.__filter_lists_by_language(source_list, target_list)
+            source_list, target_list = self.__filter_lists_by_language(
+                source_list, target_list
+            )
         return source_list, target_list
-    
+
     def __len__(self):
         return len(self.source_list)
 
     def __getitem__(self, index):
-        
+
         return None
--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,14 +1,65 @@
 import sys
+
 sys.path.append("./src")
 
-from data_utils import DataIterator
+from data_utils import DataIterator, SingleTokenizer, MTTokenizer
+
+
+def test_SingleTokenizer_initialization():
+    tokenizer = SingleTokenizer()
+    assert tokenizer.get_tokens_dictionary() == dict()
+
+
+def test_SingleTokenizer_training_model():
+    tokenizer = SingleTokenizer()
+    tokenizer.train("tests/examples/test.src")
+
+
+def test_SingleTokenizer_inference_model():
+    tokenizer = SingleTokenizer()
+
+
+def test_MTTokenizer_initialization():
+    tokenizer = MTTokenizer()
+    assert tokenizer.get_source_tokens_dictionary() == dict()
+    assert tokenizer.get_target_tokens_dictionary() == dict()
+
+
+def test_MTTokenizer_training_model():
+    tokenizer = MTTokenizer()
+    tokenizer.train("tests/examples/test.src", "tests/examples/test.tgt")
+
+
+def test_MTTokenizer_inference_model():
+    tokenizer = MTTokenizer()
+
 
 def test_generator_initialization():
-    data_iterator = DataIterator("tests/examples/test.src", "tests/examples/test.tgt", "tests/examples/test.metadata", "None")
+    data_iterator = DataIterator(
+        "tests/examples/test.src",
+        "tests/examples/test.tgt",
+        "tests/examples/test.metadata",
+        "None",
+    )
     assert len(data_iterator) == 185407
-    data_iterator = DataIterator("tests/examples/test.src", "tests/examples/test.tgt", "tests/examples/test.metadata", "en-sv")
+    data_iterator = DataIterator(
+        "tests/examples/test.src",
+        "tests/examples/test.tgt",
+        "tests/examples/test.metadata",
+        "en-sv",
+    )
     assert len(data_iterator) == 17198
-    data_iterator = DataIterator("tests/examples/test.src", "tests/examples/test.tgt", "tests/examples/test.metadata", "nb-da")
+    data_iterator = DataIterator(
+        "tests/examples/test.src",
+        "tests/examples/test.tgt",
+        "tests/examples/test.metadata",
+        "nb-da",
+    )
     assert len(data_iterator) == 19395
-    data_iterator = DataIterator("tests/examples/test.src", "tests/examples/test.tgt", "tests/examples/test.metadata", "sv-nb")
+    data_iterator = DataIterator(
+        "tests/examples/test.src",
+        "tests/examples/test.tgt",
+        "tests/examples/test.metadata",
+        "sv-nb",
+    )
     assert len(data_iterator) == 19099
