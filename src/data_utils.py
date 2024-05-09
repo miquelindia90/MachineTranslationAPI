034fdb4af12cc292d4dfaee2fa055babf8e76b59
Miquel India <mindia@verbio.com>
2024-05-10 01:57:52 +0200

    Remove Language Filtering Methods in Data Iterator

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,5 +1,6 @@
 import json
 
+
 def _read_text_sentences(file_path: str) -> list:
     """
     Read text sentences from a file.
@@ -16,7 +17,10 @@ def _read_text_sentences(file_path: str) -> list:
             sentences.append(line.strip())
     return sentences
 
-def _get_metadata_languages_indexes(metadata_file_path: str, language_filter_str: str) -> list:
+
+def _get_metadata_languages_indexes(
+    metadata_file_path: str, language_filter_str: str
+) -> list:
     """
     Retrieves the indexes of lines in a metadata file that contain a specific language filter string.
 
@@ -41,25 +45,36 @@ class SingleTokenizer:
 
     def get_tokens_dictionary(self) -> dict:
         return self._tokens_dictionary
-    
+
     def _split_sentence_in_words(self, sentence: str) -> list:
         return [word.lower() for word in sentence.strip().split()]
-    
+
     def save_tokens_dictionary(self, output_file_path: str):
         with open(output_file_path, "w") as output_file:
             json.dump(self._tokens_dictionary, output_file, indent=4)
 
-    def train(self, file_path: str, metadata_path: str = "None", language_filter_str: str = '"src_lang": "da"') -> None:
+    def train(
+        self,
+        file_path: str,
+        metadata_path: str = "None",
+        language_filter_str: str = '"src_lang": "da"',
+    ) -> None:
         self._tokens_dictionary["SOS"] = 0
         sentences = _read_text_sentences(file_path)
         if metadata_path != "None":
-            sentences = [sentences[index] for index in _get_metadata_languages_indexes(metadata_path, language_filter_str)]        
+            sentences = [
+                sentences[index]
+                for index in _get_metadata_languages_indexes(
+                    metadata_path, language_filter_str
+                )
+            ]
         for sentence in sentences:
             for word in self._split_sentence_in_words(sentence):
                 if word not in self._tokens_dictionary:
                     self._tokens_dictionary[word] = len(self._tokens_dictionary)
         self._tokens_dictionary["UNK"] = len(self._tokens_dictionary)
         self._tokens_dictionary["EOS"] = len(self._tokens_dictionary)
+
 
 class MTTokenizer:
     def __init__(self):
@@ -79,26 +94,18 @@ class DataIterator:
         source_path: str,
         target_path: str,
         metadata_path: str,
-        language_pair: str = "None",
+        language_pair_filter: str = "None",
     ):
         self._source_path = source_path
         self._target_path = target_path
         self._metadata_path = metadata_path
-        self._language_pair = language_pair
+        self._language_pair_filter = language_pair_filter
         self.source_list, self.target_list = self.__prepare_iterator_data()
 
-    def __extract_language_indexes(self) -> list:
-        language_indexes = list()
-        languages = self._language_pair.split("-")
-        with open(self._metadata_path, "r") as metadata_file:
-            for index, line in enumerate(metadata_file.readlines()):
-                sline = line.strip().split('"')
-                if sline[3] == languages[0] and sline[7] == languages[1]:
-                    language_indexes.append(index)
-        return language_indexes
-
     def __filter_lists_by_language(self, source_list: list, target_list: list) -> tuple:
-        language_indexes = self.__extract_language_indexes()
+        language_indexes = _get_metadata_languages_indexes(
+            self._metadata_path, self._language_pair_filter
+        )
         indexed_source_list = [source_list[index] for index in language_indexes]
         indexed_target_list = [target_list[index] for index in language_indexes]
         return indexed_source_list, indexed_target_list
@@ -106,7 +113,7 @@ class DataIterator:
     def __prepare_iterator_data(self):
         source_list = _read_text_sentences(self._source_path)
         target_list = _read_text_sentences(self._target_path)
-        if self._language_pair != "None":
+        if self._language_pair_filter != "None":
             source_list, target_list = self.__filter_lists_by_language(
                 source_list, target_list
             )
--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -14,15 +14,20 @@ def test_SingleTokenizer_initialization():
 
 def test_SingleTokenizer_training_model():
     tokenizer = SingleTokenizer()
-    tokenizer.train("tests/examples/test.src", "tests/examples/test.metadata", '"src_lang": "nb"')
+    tokenizer.train(
+        "tests/examples/test.src", "tests/examples/test.metadata", '"src_lang": "nb"'
+    )
     tokenizer_dictionary = tokenizer.get_tokens_dictionary()
     assert tokenizer_dictionary["SOS"] == 0
     assert tokenizer_dictionary["UNK"] == 104452
     tokenizer.save_tokens_dictionary("tests/examples/output_tokens.json")
-    with open("tests/examples/reference_tokens.json", "r") as target_file,  open("tests/examples/output_tokens.json", "r") as output_file:
+    with open("tests/examples/reference_tokens.json", "r") as target_file, open(
+        "tests/examples/output_tokens.json", "r"
+    ) as output_file:
         target_dict = json.load(target_file)
         output_dict = json.load(output_file)
         assert target_dict == output_dict
+
 
 def test_SingleTokenizer_inference_model():
     tokenizer = SingleTokenizer()
@@ -55,20 +60,20 @@ def test_generator_initialization():
         "tests/examples/test.src",
         "tests/examples/test.tgt",
         "tests/examples/test.metadata",
-        "en-sv",
+        '"src_lang": "en", "tgt_lang": "sv"',
     )
     assert len(data_iterator) == 17198
     data_iterator = DataIterator(
         "tests/examples/test.src",
         "tests/examples/test.tgt",
         "tests/examples/test.metadata",
-        "nb-da",
+        '"src_lang": "nb", "tgt_lang": "da"',
     )
     assert len(data_iterator) == 19395
     data_iterator = DataIterator(
         "tests/examples/test.src",
         "tests/examples/test.tgt",
         "tests/examples/test.metadata",
-        "sv-nb",
+        '"src_lang": "sv", "tgt_lang": "nb"',
     )
     assert len(data_iterator) == 19099
