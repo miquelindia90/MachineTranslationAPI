6199bb7a7237e0bf2b79acd8eb63ddaa3eaacdff
Miquel India <mindia@verbio.com>
2024-05-09 20:55:39 +0200

    Added Data Initialization without Tokenization

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,9 +1,46 @@
 
 
+def _read_text_sentences(file_path: str)-> list:
+    sentences = list()
+    with open(file_path, 'r') as input_file:
+        for line in input_file:
+            sentences.append(line.strip())
+    return sentences
+
 
 class DataIterator():
-    def __init__(self, source_path: str, target_path: str, metadata_path: str, language_pair: str):
+    def __init__(self, source_path: str, target_path: str, metadata_path: str, language_pair: str = "None"):
         self._source_path = source_path
         self._target_path = target_path
         self._metadata_path = metadata_path
         self._language_pair = language_pair
+        self.source_list, self.target_list = self.__prepare_iterator_data()
+        
+
+    def __extract_language_indexes(self) -> list:
+        language_indexes = list()
+        languages = self._language_pair.split("-")        
+        with open(self._metadata_path, 'r') as metadata_file:
+            for line in metadata_file:
+                sline = line.strip().split('"')
+                if sline[3] == languages[0] and sline[7] == languages[1]:
+                    language_indexes.append(sline[0])
+        return language_indexes
+
+    def __filter_lists_by_language(self, source_list: list, target_list: list) -> tuple:
+        language_indexes = self.__extract_language_indexes()
+        return source_list, target_list
+
+    def __prepare_iterator_data(self):
+        source_list = _read_text_sentences(self._source_path)
+        target_list = _read_text_sentences(self._target_path)
+        if self._language_pair != "None":
+            source_list, target_list = self.__filter_lists_by_language(source_list, target_list)
+        return source_list, target_list
+    
+    def __len__(self):
+        return len(self.source_list)
+
+    def __getitem__(self, index):
+        
+        return None
--- tests/test_generator_methods.py
+++ tests/test_generator_methods.py
@@ -4,4 +4,5 @@ sys.path.append("./src")
 from data_utils import DataIterator
 
 def test_generator_initialization():
-    data_iterator = DataIterator("examples/test.src", "examples/test.tgt", "examples/test.metadata", "en-sv")
+    data_iterator = DataIterator("tests/examples/test.src", "tests/examples/test.tgt", "tests/examples/test.metadata", "en-sv")
+    assert len(data_iterator) == 185407
