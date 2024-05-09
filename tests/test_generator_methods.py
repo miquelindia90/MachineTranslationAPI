6f5b7fa59c5ab5800d8a942bb086bbe9ff2459b4
Miquel India <mindia@verbio.com>
2024-05-09 19:52:22 +0200

    Added Init Test

--- tests/test_generator_methods.py
+++ tests/test_generator_methods.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.append("./src")
 
+from data_utils import DataIterator
 
-def test_generator_methods():
-
-    assert 1==1
+def test_generator_initialization():
+    data_iterator = DataIterator("examples/test.src", "examples/test.tgt", "examples/test.metadata", "en-sv")
