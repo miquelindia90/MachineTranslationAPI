9616761932c00f4c072686ae5812e3fa43cc78ed
Miquel India <mindia@verbio.com>
2024-05-11 13:07:55 +0200

    Added Tests for MT Toeknizer

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -49,10 +49,33 @@ def test_MTTokenizer_initialization():
     assert tokenizer.get_target_tokens_dictionary() == dict()
 
 
-# def test_MTTokenizer_training_model():
-#     tokenizer = MTTokenizer()
-#     tokenizer.train("tests/examples/test.src", "tests/examples/test.tgt")
+def test_MTTokenizer_training_model():
+    tokenizer = MTTokenizer()
+    tokenizer.train("tests/examples/test.src", "tests/examples/test.tgt", "tests/examples/test.metadata", '"src_lang": "en", "tgt_lang": "sv"')
+    source_tokens_dictionary = tokenizer.get_source_tokens_dictionary()
+    target_tokens_dictionary = tokenizer.get_target_tokens_dictionary()
+    assert source_tokens_dictionary["SOS"] == 0
+    assert source_tokens_dictionary["UNK"] == 668
+    assert target_tokens_dictionary["SOS"] == 0
+    assert target_tokens_dictionary["UNK"] == 675
+    tokenizer.save_tokens_dictionary("tests/examples/output_source_tokens.json", "tests/examples/output_target_tokens.json")
+    with open("tests/examples/reference_source_tokens.json", "r") as target_file, open("tests/examples/output_source_tokens.json", "r") as output_file:
+        target_dict = json.load(target_file)
+        output_dict = json.load(output_file)
+        assert target_dict == output_dict
+    os.remove("tests/examples/output_source_tokens.json")
+    with open("tests/examples/reference_target_tokens.json", "r") as target_file, open("tests/examples/output_target_tokens.json", "r") as output_file:
+        target_dict = json.load(target_file)
+        output_dict = json.load(output_file)
+        assert target_dict == output_dict
+    os.remove("tests/examples/output_target_tokens.json")
 
-
-# def test_MTTokenizer_inference_model():
-#     tokenizer = MTTokenizer()
+def test_MTTokenizer_inference_model():
+    tokenizer = MTTokenizer()
+    tokenizer.load_tokens_dictionary("tests/examples/reference_source_tokens.json", "tests/examples/reference_target_tokens.json")
+    assert tokenizer.source_lang_word_to_id("UNK") == 668
+    assert tokenizer.source_lang_word_to_id("SOS") == 0
+    assert tokenizer.target_lang_word_to_id("UNK") == 675
+    assert tokenizer.target_lang_word_to_id("SOS") == 0
+    assert tokenizer.source_lang_sentence_to_id_list("And it is also the case") == [1, 2, 3, 4, 5, 6]
+    assert tokenizer.target_lang_sentence_to_id_list("Så är det även i dag med") == [1, 2, 3, 4, 5, 6, 7]
