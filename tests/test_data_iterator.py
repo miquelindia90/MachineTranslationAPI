f79f2a7b5a2db5206398db513d86bd324989f266
Miquel India <mindia@verbio.com>
2024-05-13 11:21:01 +0200

    Improve Testing Structure

--- tests/test_data_iterator.py
+++ tests/test_data_iterator.py
@@ -3,34 +3,23 @@ import sys
 sys.path.append("./src")
 
 from data_iterator import DataIterator
-
+from tokenizer import MTTokenizer
 
 def test_generator_initialization():
-    data_iterator = DataIterator(
-        "tests/examples/test.src",
-        "tests/examples/test.tgt",
-        "tests/examples/test.metadata",
-        "None",
-    )
-    assert len(data_iterator) == 1000
-    data_iterator = DataIterator(
-        "tests/examples/test.src",
-        "tests/examples/test.tgt",
-        "tests/examples/test.metadata",
-        '"src_lang": "en", "tgt_lang": "sv"',
-    )
-    assert len(data_iterator) == 83
-    data_iterator = DataIterator(
-        "tests/examples/test.src",
-        "tests/examples/test.tgt",
-        "tests/examples/test.metadata",
-        '"src_lang": "nb", "tgt_lang": "da"',
-    )
-    assert len(data_iterator) == 108
-    data_iterator = DataIterator(
-        "tests/examples/test.src",
-        "tests/examples/test.tgt",
-        "tests/examples/test.metadata",
-        '"src_lang": "sv", "tgt_lang": "nb"',
-    )
-    assert len(data_iterator) == 100
+    test_source_path = "tests/examples/test.src"
+    test_target_path = "tests/examples/test.tgt"
+    test_metadata_path = "tests/examples/test.metadata"
+    language_pairs = ["None", '"src_lang": "en", "tgt_lang": "sv"', '"src_lang": "nb", "tgt_lang": "da"', '"src_lang": "sv", "tgt_lang": "nb"']
+    data_iterator_lengths = [1000, 83, 108, 100]
+
+    for language_pair, data_iterator_length in zip(language_pairs, data_iterator_lengths):
+        tokenizer = MTTokenizer()    
+        data_iterator = DataIterator(
+        test_source_path,
+        test_target_path,
+        test_metadata_path,
+        language_pair
+        )
+        assert len(data_iterator) == data_iterator_length
+
+    
