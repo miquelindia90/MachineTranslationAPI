fe8bad8b478b8e13c8fd2b0768256e70a307d947
mindia <mindia@traceventures.es>
2024-05-13 14:24:55 +0200

    Extended Iterator to consider SOS and EOS

--- src/data_iterator.py
+++ src/data_iterator.py
@@ -61,4 +61,6 @@ class DataIterator:
         return len(self._tokenized_source_list)
 
     def __getitem__(self, index):
-        return self._tokenized_source_list[index], self._tokenized_target_list[index]
+        source_item_list = [self._tokenizer.source_lang_word_to_id("SOS")] + self._tokenized_source_list[index] + [self._tokenizer.source_lang_word_to_id("EOS")]
+        target_item_list = [self._tokenizer.target_lang_word_to_id("SOS")] + self._tokenized_target_list[index] + [self._tokenizer.target_lang_word_to_id("EOS")]
+        return source_item_list, target_item_list
