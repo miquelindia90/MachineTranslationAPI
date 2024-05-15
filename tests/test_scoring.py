c0eab731cd90feb71748fd7ae8b328269fb946a4
Miquel India <mindia@verbio.com>
2024-05-15 11:31:00 +0200

    Added Tests for bath bleu scoring methods

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -1,12 +1,46 @@
 import sys
-import pytest
+import torch
 
 sys.path.append("./src")
 
-from data.scoring import calculate_bleu_score
+from data.tokenizer import MTTokenizer
+from data.scoring import calculate_bleu_score, calculate_batch_bleu_score
+
 
 def test_bleu_calculation_methods():
     candidate_translation = "There is a cat in the mat"
-    reference_translations = ["The cat is sitting on the mat", "There is a cat on the mat"]
+    reference_translations = [
+        "The cat is sitting on the mat",
+        "There is a cat on the mat",
+    ]
     assert calculate_bleu_score(candidate_translation, reference_translations) == 48.89
 
+
+def test_batch_bleu_calculation():
+    tokenizer = MTTokenizer()
+    tokenizer.load_tokens_dictionary(
+        "tests/examples/reference_source_tokens.json",
+        "tests/examples/reference_target_tokens.json",
+    )
+
+    target_tensor = torch.Tensor([[0, 1, 2, 3]])
+    target_lengths = torch.Tensor([4]).long()
+    output_tensor = torch.Tensor(
+        [
+            [
+                [0.1, 0.2, 0.3, 0.4],
+                [0.2, 0.3, 0.4, 0.1],
+                [0.3, 0.1, 0.4, 0.2],
+                [0.1, 0.3, 0.2, 0.4],
+                [0.1, 0.3, 0.2, 0.4],
+            ]
+        ]
+    )
+    assert (
+        calculate_batch_bleu_score(
+            output_tensor, target_tensor, target_lengths, tokenizer
+        )
+        == 0.0
+    )
+
+    # TO DO: Add more test cases
